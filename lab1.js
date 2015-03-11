84b303d5bf22e5fa49309b8c847f14c202da871b
oneilit <oneilit@clarkson.edu>
2015-03-11 17:58:49 -0400

    Completed lab1.js

--- lab1.js
+++ lab1.js
@@ -1,6 +1,7 @@
 
 var gl;
 var points;
+var index;
 
 window.onload = function init()
 {
@@ -9,7 +10,9 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    var tri_vertices = new Float32Array([0, 0, 0, 0.75, 0.75, 0.75])
+    var tri_vertices = new Float32Array([-0.5, -.5, 0, 0.75, 0.5, -0.5]);
+	var squ_vertices = new Float32Array([0.5,0.5,0.5,-0.5,-0.5,-0.5,-.5,.5]);
+	var pen_vertices = new Float32Array([-.4, -.7, -.6, 0, 0, .5, .6, 0, .4, -.7]);
     //
     //  Configure WebGL
     //
@@ -30,10 +33,37 @@ window.onload = function init()
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
     
+   index=1;
+	render();
     // Associate our shader variables with our data buffer
-    index=0;
-    canvas.addEventListener("click", function(){index=index+1; if (index==4){index=1;}});
-    render();
+ 
+    canvas.addEventListener("click", function(){index=index+1;
+		//if (index == 1){
+			//var buffer = gl.createBuffer();
+			//gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+			//gl.bufferData(gl.ARRAY_BUFFER, tri_vertices, gl.STATIC_DRAW);}
+		
+		if (index == 2){
+			var buffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+			gl.bufferData(gl.ARRAY_BUFFER, squ_vertices, gl.STATIC_DRAW);}
+
+		else if (index == 3){
+			var buffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+			gl.bufferData(gl.ARRAY_BUFFER, pen_vertices, gl.STATIC_DRAW);}
+			
+		else if (index==4){index=1;
+			var buffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+			gl.bufferData(gl.ARRAY_BUFFER, tri_vertices, gl.STATIC_DRAW);}
+			
+		var vPosition = gl.getAttribLocation( program, "vPosition");
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+	render();
+	}
+	);
 };
 
 
@@ -43,9 +73,9 @@ function render() {
         gl.drawArrays(gl.TRIANGLES, 0, 3);
     }
     else if (index == 2){
-        gl.drawArrays(gl.LINE_LOOP, 0, 0, 0.5, 1, 1, 0);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
     }
     else{
-        gl.drawArrays(gl.LINE_LOOP, 0, 0, 0, 0.5, 0.5, 1, 1, 0.5, 1, 0)
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 5);
     }
 }
